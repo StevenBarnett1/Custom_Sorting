33f2ea65409775fdc5ce9f0e627e7cd4c1cb9bc1
Derek Jou <derekjou0@gmail.com>
2021-02-05 15:47:57 -0800

    initial commit, tuesday

---
+++ practice/assorted-sorts.js
@@ -0,0 +1,38 @@
+const oddEvenSort = arr => {
+
+};
+
+const reverseBaseSort = arr => {
+
+};
+
+const snakeSort = arr => {
+
+};
+
+const relativeSort = (arr1, arr2) => {
+
+};
+
+const frequencySort = arr => {
+
+};
+
+const largestNumber = arr => {
+
+};
+
+const strengthSort = arr => {
+
+};
+
+
+module.exports = {
+    oddEvenSort,
+    reverseBaseSort,
+    snakeSort,
+    relativeSort,
+    frequencySort,
+    largestNumber,
+    strengthSort
+}
---
+++ practice/merge-and-quick-sort.js
@@ -0,0 +1,22 @@
+// Merge Sort and Merge Helper function
+const mergeSort = (arr) => {
+
+};
+
+const merge = (arr1, arr2) => {
+
+};
+
+
+
+// Quick Sort
+const quickSort = (arr) => {
+
+};
+
+
+module.exports = {
+    merge,
+    mergeSort,
+    quickSort
+}
